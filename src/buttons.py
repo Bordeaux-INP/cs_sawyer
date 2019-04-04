1040fb74855654afb1a69d7fd63a3016bf1592f2
Yoan Mollard <ymollard@users.noreply.github.com>
2019-04-04 15:01:55 +0200

    Lights update

--- src/buttons.py
+++ src/buttons.py
@@ -24,7 +24,7 @@ class Buttons(object):
     SLOW_BLINK_DURATION = 1     # Second of blinking on and off
 
     def __init__(self):
-        self.last_pressed_time = rospy.Time.now()
+        self.last_pressed_time = rospy.Time(0)
         self.publisher = rospy.Publisher("cs_sawyer/button", ButtonPressed, queue_size=1)
         self.fear_led_status = LightStatus.OFF
         self.hope_led_status = LightStatus.OFF
@@ -45,10 +45,10 @@ class Buttons(object):
             rospy.logwarn("Node hasn't found the GPIO, buttons will not work")
 
     def _fear_light_update(self, msg):
-        self.fear_led_status = msg.type
+        self.fear_led_status = msg.type.data
 
     def _hope_light_update(self, msg):
-        self.hope_led_status = msg.type
+        self.hope_led_status = msg.type.data
 
     def pressed(self, button_id):
         if gpio_available:
@@ -60,38 +60,38 @@ class Buttons(object):
         if gpio_available:
             now = rospy.Time.now()
             # Hope LED update
-            if self._hope_light_update == LightStatus.ON:
+            if self.hope_led_status == LightStatus.ON:
+                rospy.loginfo("HOPE ON")
                 GPIO.output(self.PIN_LED_HOPE, True)
                 self.last_hope_led_update_time = rospy.Time.now()
-            elif self._hope_light_update == LightStatus.OFF:
+            elif self.hope_led_status == LightStatus.OFF:
                 GPIO.output(self.PIN_LED_HOPE, False)
                 self.last_hope_led_update_time = rospy.Time.now()
-            elif self._hope_light_update == LightStatus.FAST_BLINK:
-                if now > self.last_hope_led_update_time + self.FAST_BLINK_DURATION:
+            elif self.hope_led_status == LightStatus.FAST_BLINK:
+                if now > self.last_hope_led_update_time + rospy.Duration(self.FAST_BLINK_DURATION):
                     self._hope_led_on = not self._hope_led_on
                     GPIO.output(self.PIN_LED_HOPE, self._hope_led_on)
                     self.last_hope_led_update_time = now
-            elif self._hope_light_update == LightStatus.SLOW_BLINK:
-                if now > self.last_hope_led_update_time + self.SLOW_BLINK_DURATION:
+            elif self.hope_led_status == LightStatus.SLOW_BLINK:
+                if now > self.last_hope_led_update_time + rospy.Duration(self.SLOW_BLINK_DURATION):
                     self._hope_led_on = not self._hope_led_on
                     GPIO.output(self.PIN_LED_HOPE, self._hope_led_on)
                     self.last_hope_led_update_time = now
-            else:
-                rospy.loginfo("No LED change")
+
             # Fear LED update
-            if self._fear_light_update == LightStatus.ON:
+            if self.fear_led_status == LightStatus.ON:
                 GPIO.output(self.PIN_LED_FEAR, True)
                 self.last_fear_led_update_time = rospy.Time.now()
-            elif self._fear_light_update == LightStatus.OFF:
+            elif self.fear_led_status == LightStatus.OFF:
                 GPIO.output(self.PIN_LED_FEAR, False)
                 self.last_fear_led_update_time = rospy.Time.now()
-            elif self._fear_light_update == LightStatus.FAST_BLINK:
-                if now > self.last_fear_led_update_time + self.FAST_BLINK_DURATION:
+            elif self.fear_led_status == LightStatus.FAST_BLINK:
+                if now > self.last_fear_led_update_time + rospy.Duration(self.FAST_BLINK_DURATION):
                     self._fear_led_on = not self._fear_led_on
                     GPIO.output(self.PIN_LED_FEAR, self._fear_led_on)
                     self.last_fear_led_update_time = now
-            elif self._fear_light_update == LightStatus.SLOW_BLINK:
-                if now > self.last_fear_led_update_time + self.SLOW_BLINK_DURATION:
+            elif self.fear_led_status == LightStatus.SLOW_BLINK:
+                if now > self.last_fear_led_update_time + rospy.Duration(self.SLOW_BLINK_DURATION):
                     self._fear_led_on = not self._fear_led_on
                     GPIO.output(self.PIN_LED_FEAR, self._fear_led_on)
                     self.last_fear_led_update_time = now
