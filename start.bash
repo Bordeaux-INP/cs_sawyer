331afaa7271a2ecfba5af3751ba9b0af57a7f08c
Yoan Mollard <ymollard@users.noreply.github.com>
2019-04-04 15:34:16 +0200

    Update start.bash

--- start.bash
+++ start.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-master_hostname="021607CP00116.local"
+master_hostname="021607CP00116"
 hostname=`hostname`
 
-env_vars="export ROS_MASTER_URI=\"http://${master_hostname}:11311\" 
+env_vars="export ROS_MASTER_URI=\"http://${master_hostname}.local:11311\" 
 export ROS_HOSTNAME=\"${hostname}.local\" 
 export PS1=\"\[\033[00;33m\][\${ROS_MASTER_URI}]\[\033[00m\] \${PS1}\""
 
